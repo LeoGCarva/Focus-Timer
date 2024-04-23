724b278da964d25d502a6524e9bcef7f1bbda292
Leonardo <leu1.g.carvalho@gmail.com>
2024-04-22 22:13:35 -0300

    first commit

---
+++ src/FocusTimer/actions.js
@@ -0,0 +1,22 @@
+import state from './state.js'
+import * as timer from './timer.js'
+import * as el from './elementes.js'
+
+export function toggleRunning() {
+    state.isRunning = document.documentElement.classList.toggle('running')
+    timer.countdown()
+}
+
+export function reset() {
+    state.isRunning = false;
+    document.documentElement.classList.remove('running');
+}
+
+export function set() {
+    el.minutes.setAttribute('contenteditable', true)
+    el.minutes.focus()
+}
+
+export function toggleMusic() {
+    state.isMute = document.documentElement.classList.toggle('music-on');
+}
